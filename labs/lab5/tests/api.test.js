e3d8ba9a8f3a9ad4771fd55474b95672fa90652c
ewheeler <145588852+ewheeler19@users.noreply.github.com>
2025-03-21 13:54:18 -0300

    done

--- labs/lab5/tests/api.test.js
+++
@@ -1,8 +0,0 @@
-const request = require("supertest");
-const app = require("./api.js");
-
-test("should return recipes", async () => {
-    const res = await request(app).get("/api/recipes?ingredients=tomato&diet=vegetarian");
-    expect(res.statusCode).toBe(200);
-    expect(res.body.length).toBeGreaterThan(0);
-});
